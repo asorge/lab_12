3f00a1bc28064698874c5a74998914e3fc7ee2b4
Ally Sorge <asorge@andrew.cmu.edu>
2016-05-16 13:27:43 -0400

    created sessions controller and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ BandBlitz::Application.routes.draw do
   # Generated routes
   resources :bands
   resources :genres
+  resources :users
+  resources :sessions
+  get 'user/edit' => 'users#edit', :as => :edit_current_user
+  get 'signup' => 'users#new', :as => :signup
+  get 'login' => 'sessions#new', :as => :login
+  get 'logout' => 'sessions#destroy', :as => :logout
 
   # Default route
   root :to => 'bands#index', :as => :home
